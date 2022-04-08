d9501d2fd203fac2183fbe51fad472bea8da4daf
brunoleomont <brunoleomont@gmail.com>
2022-04-08 16:03:25 -0300

    Initial Commit

---
+++ src/main/java/com/wayapp/wayappmanager/WayappmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.wayapp.wayappmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WayappmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WayappmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wayapp/wayappmanager/WayappmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wayapp.wayappmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WayappmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
